2b90cba142f6c8e263ff8cc34d4ff833f59a3476
Tran Trong Nghia (Victor) <trantrongnghia.vn@smilegate.com>
2025-12-01 10:58:17 +0700

    video 31: how to use variables in JSX

--- 01-react-vite-starter/src/components/learn/FirstComponent.jsx
+++ 01-react-vite-starter/src/components/learn/FirstComponent.jsx
@@ -5,16 +5,32 @@
 import './style.css';
 
 const FirstComponent = () => {
+
+  // *** [Primitive data types] ***
+  // const name = 'Victor Tran';
+  // const age = 32;
+  // const isAMen = true;
+  // const undefinedValue = undefined;
+  // const nullValue = null;
+
+  // *** [Reference data types] ***
+  // const arr = [1, 2, 3, 4, 5];
+  const personInfo = {
+    name: 'Victor Tran',
+    age: 32,
+    isAMen: true
+  };
   return (
     <>
       {/* React Fragment:
         - option 1: use <React.Fragment></React.Fragment>
         - option 2: use <></> (recommendations)
       */}
-      <div className="child-1">1st component</div>
+      <div className="child-1">Variable Value: {JSON.stringify(personInfo)}</div>
+      <div>{console.log('>>> Test code js in html <<<')}</div>
 
       {/* inline style */}
-      <div style={{borderRadius: "5px", border: "1px solid black"}}>1.1 component</div>
+      <div style={{borderRadius: "5px", border: "1px solid black"}}>1st component</div>
     </>
   );
 }

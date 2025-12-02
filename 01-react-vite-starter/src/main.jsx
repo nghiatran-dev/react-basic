d6d28f25163ef47968c6ac5b179b65403995fd2a
Tran Trong Nghia (Victor) <trantrongnghia.vn@smilegate.com>
2025-12-02 10:37:29 +0700

    video 45: integrated router

--- 01-react-vite-starter/src/main.jsx
+++ 01-react-vite-starter/src/main.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <div>Login Page</div>,
+  },
+  {
+    path: "/register",
+    element: <div>Register Page</div>,
+  },
+  {
+    path: "/user",
+    element: <div>User Page</div>,
+  },
+  {
+    path: "/product",
+    element: <div>Product Page</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

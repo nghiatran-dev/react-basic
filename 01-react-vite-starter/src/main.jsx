1ea8732fa218f3db2f562355239dd2785d877f39
Tran Trong Nghia (Victor) <trantrongnghia.vn@smilegate.com>
2025-12-02 14:08:30 +0700

    video 52: handle route not found

--- 01-react-vite-starter/src/main.jsx
+++ 01-react-vite-starter/src/main.jsx
@@ -9,13 +9,15 @@ import TodoApp from './components/todo/TodoApp.jsx';
 import LoginPage from './pages/login.jsx';
 import RegisterPage from './pages/register.jsx';
 import UserPage from './pages/user.jsx';
-import ProductPage from './pages/product.jsx';
+import BookPage from './pages/book.jsx';
+import ErrorPage from './pages/error.jsx';
 import './styles/global.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -26,8 +28,8 @@ const router = createBrowserRouter([
         element: <UserPage />,
       },
       {
-        path: "products",
-        element: <ProductPage />,
+        path: "books",
+        element: <BookPage />,
       }
     ],
   },

662cac61404c562d9b37ce9df3aede88a06cbef6
Tran Trong Nghia (Victor) <trantrongnghia.vn@smilegate.com>
2025-12-08 10:42:24 +0700

    video 91: responsive register screen

--- 01-react-vite-starter/src/pages/register.jsx
+++ 01-react-vite-starter/src/pages/register.jsx
@@ -1,4 +1,4 @@
-import { Flex, Form, Button, Input, notification } from 'antd';
+import { Row, Col, Flex, Form, Button, Input, notification } from 'antd';
 import { apiRegister } from '../services/api.service';
 import { useNavigate } from 'react-router-dom';
 
@@ -26,20 +26,24 @@ const RegisterPage = () => {
         console.log('Failed:', errorInfo);
     };
     return (
-        <Flex justify="center" align='center' style={{ marginTop: 50, marginBottom: 20 }} vertical>
-            <div style={{width: "40%"}}>
-                <h1 style={{marginBottom: "20px"}}>Register</h1>
-                <Flex style={{width: "100%"}}>
-                    <Form
-                        name="register"
-                        labelCol={{ span: 8 }}
-                        labelAlign="left"
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                        style={{width: "100%"}}
-                    >
+        <Flex justify="center" style={{ marginTop: 50, marginBottom: 20, width: "100%" }} vertical>
+            <Row justify={'center'} style={{margin: "10px"}}>
+                <Col xs={24} md={12}>
+                    <h1 style={{marginBottom: "20px", marginLeft: "5px"}}>Register</h1>
+                </Col>
+            </Row>
+            <Form
+                name="register"
+                labelCol={{ span: 8 }}
+                labelAlign="left"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+                style={{margin: "20px"}}
+            >
+                <Row justify={'center'}>
+                    <Col xs={24} md={12}>
                         <Form.Item
                             label="Full name"
                             name="fullName"
@@ -47,7 +51,11 @@ const RegisterPage = () => {
                         >
                             <Input />
                         </Form.Item>
+                    </Col>
+                </Row>
 
+                <Row justify={'center'}>
+                    <Col xs={24} md={12}>
                         <Form.Item
                             label="Email"
                             name="email"
@@ -58,7 +66,11 @@ const RegisterPage = () => {
                         >
                             <Input />
                         </Form.Item>
+                    </Col>
+                </Row>
 
+                <Row justify={'center'}>
+                    <Col xs={24} md={12}>
                         <Form.Item
                             label="Password"
                             name="password"
@@ -69,7 +81,11 @@ const RegisterPage = () => {
                         >
                             <Input.Password />
                         </Form.Item>
+                    </Col>
+                </Row>
 
+                <Row justify={'center'}>
+                    <Col xs={24} md={12}>
                         <Form.Item
                             label="Phone number"
                             name="phone"
@@ -80,16 +96,20 @@ const RegisterPage = () => {
                         >
                             <Input maxLength={10}/>
                         </Form.Item>
+                    </Col>
+                </Row>
 
+                <Row justify={'center'}>
+                    <Col xs={24} md={12}>
                         <Form.Item 
                             label={null}
                             wrapperCol={{ offset: 8, span: 16 }}>
                             <Button type="primary" htmlType="submit">Register</Button>
                         </Form.Item>
-                    </Form>
-                </Flex>
-            </div>
-        </Flex>
+                    </Col>
+                </Row>
+            </Form>
+        </Flex> 
     )
 }
 
